ff298a70d01bcdb510551c994b9bca86029854d6
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2023-08-02 02:29:42 +0530

    changes done

---
+++ README.md
@@ -0,0 +1,10 @@
+Process for running the code: 
+Step 1: Download the zip file that is present in the repo and after that extract the files.
+Step 2: Open the terminal and add this command "npm install", This will make sure that all the dependencies
+are installed correctly.
+Step 3: Run "npm start" in the terminal to get started with the Live Project.
+
+Another Process can be: 
+Get the URL of clone.
+Then open an empty VS Code folder and run git clone "URL".
+Then Run npm install and npm start to get Started.
---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +91 8707844732</label>
+            <i className='fa fa-envelope'></i>
+            <label>shivamkrsingh4688@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span></span>
+            <label>EN</label>
+            <span></span>
+            <label>INR</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
